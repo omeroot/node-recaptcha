30be7376ba082ea9057256ac55fc6ade17ff252f
omeroot <demircanomer91@gmail.com>
2015-10-27 14:37:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -25,28 +25,18 @@ app.get('/form', function (req, res) {
 
 app.post('/form', function (req, res) {
   var cpt = req.body["g-recaptcha-response"];
-<<<<<<< HEAD
   var form = {
-    "secret": "YOUR SECRET",
+    "secret": opt.secret,
     "response": cpt
   };
 
   request.post({url: opt.hostname, form: form}, function (err, res) {
     if (err) throw err;
     console.log("success:", res.body.success);
-=======
-  request.post({url:opt.hostname,form:{"secret":opt.secret,"response":cpt}},function(err,res,body){
-    if(err) throw err;
-    console.log("success:",res.body);
->>>>>>> 4ec48324d87971f16b6eb97a29df869aa8d1d9ad
   });
 });
 
 
-<<<<<<< HEAD
 server.listen(5050, function () {
   console.log('listening 5050')
 });
-=======
-server.listen(5050,function(){console.log('listening 5050')});
->>>>>>> 4ec48324d87971f16b6eb97a29df869aa8d1d9ad
